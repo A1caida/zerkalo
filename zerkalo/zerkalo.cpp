0c9bf6390a94eea77582a7f6415fa3a490bd6f50
A1caida <46443450+A1caida@users.noreply.github.com>
2019-12-24 17:15:16 +0400

    Add files via upload

---
+++ zerkalo/zerkalo.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, c=0;
+	double temp,m=0;
+
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Введите число" << endl;
+	cin >> a;
+	cout << endl;
+	while (a!=0)
+	{
+		temp = a % 10;
+		a = a / 10;
+		m = m + temp;
+		m = m * 10;
+		c++;
+
+	}
+	m=m/10;
+	cout << m;
+}
+
+
+
